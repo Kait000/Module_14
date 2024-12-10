dd93316be9bc948326d5e89e67c48c38e8046d2f
Роман <x-sel@Rambler.ru>
2024-12-10 11:24:48 +0500

    Add files via upload

---
+++ Module_14_01/module_14_1.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+db = sqlite3.connect('not_telegram.db')  # устанавливаем соединение с БД
+cursor = db.cursor()
+
+"""Создаем таблицу Users"""
+query = """CREATE TABLE IF NOT EXISTS Users(
+    id INTEGER PRIMARY KEY, 
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    age INTEGER,
+    balance INTEGER NOT NULL
+)"""
+cursor.execute(query)
+db.commit()
+
+"""Составляем список кортежей для добавления в таблицу"""
+data = []
+for i in range(1, 11):
+    data.append((f'User{i}', f'example{i}@gmail.com', i*10, 1000))
+"""Добавляем записи в таблицу"""
+cursor.executemany("""INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)""", data)
+db.commit()
+
+"""Составляем свой список id таблицы Users, т.к. в рабочих БД индексы могут идти не попорядку"""
+data_id = []    # содержит список id всех записей
+query = """SELECT id FROM Users ORDER BY id"""
+for i in cursor.execute(query):
+    data_id.append(i[0])
+
+"""Для каждой второй записи меняем balance на 500"""
+for i in range(0, len(data_id), 2):
+    cursor.execute("""UPDATE Users SET balance = ? WHERE id = ?""", (500, i+1))
+db.commit()
+
+"""Удаляем каждую третью запись"""
+for i in range(0, len(data_id), 3):
+    cursor.execute("""DELETE FROM Users WHERE id = ?""", (i+1,))
+db.commit()
+
+"""Делаем выборку всех записей где возраст != 60"""
+cursor.execute("""SELECT * FROM Users WHERE age != ?""", (60,))
+for i in cursor.fetchall():
+    print(f'Имя: {i[1]} | Почта: {i[2]} | Возраст: {i[3]} | Баланс: {i[4]}')
+
+db.close()
